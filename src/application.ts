7d458d994b0b43f1cbb0e03707abcbfddcceb390
Matt Adams <matt@offhourscoding.com>
2019-05-19 20:25:54 -0400

    start db based off environment vars or project settings

--- src/application.ts
+++ src/application.ts
@@ -9,12 +9,33 @@ import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import * as path from 'path';
 import {MySequence} from './sequence';
+import {DbDataSource} from './datasources';
 
 export class TodoListApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
+
+    // Set datasource based off environment
+    const db_host = process.env.MONGO_HOST || 'localhost';
+    const db_port = process.env.MONGO_PORT || 27017;
+    const db_user = process.env.MONGO_USERNAME || '';
+    const db_pass = process.env.MONGO_PASSWORD || '';
+    const database = process.env.MONGO_DB || 'todo-list';
+
+    this.bind('datasources.config.db').to({
+      name: 'db',
+      connector: 'mongodb',
+      url: '',
+      host: db_host,
+      port: db_port,
+      user: db_user,
+      password: db_pass,
+      database: database,
+      useNewUrlParser: true,
+    });
+    this.bind('datasources.db').toClass(DbDataSource);
 
     // Set up the custom sequence
     this.sequence(MySequence);
